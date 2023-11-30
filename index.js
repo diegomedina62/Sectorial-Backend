8e20d7676c7eb3fcc3767267f782ce0c17c0b491
Diego Medina <diegoleonmedina62@gmail.com>
2023-11-30 02:32:57 -0300

    cors funcionality

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-require("dotenv").config()
-const express = require("express")
+require('dotenv').config()
+const express = require('express')
 const app = express()
+const cors = require('cors')
 
 //import Routers
-const categoriesRouter = require("./routes/categories")
-const subcategoriesRouter = require("./routes/subcategories")
-const subjectsRouter = require("./routes/subjects")
+const categoriesRouter = require('./routes/categories')
+const subcategoriesRouter = require('./routes/subcategories')
+const subjectsRouter = require('./routes/subjects')
 
 //middlewares
 app.use(express.json())
+app.use(cors())
 
 //initial route
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send(
-    "Bienvenido a la API de gestion de SQL para sectorial, by Diego Medina"
+    'Bienvenido a la API de gestion de SQL para sectorial, by Diego Medina'
   )
 })
 
 //routes
-app.use("/categories", categoriesRouter)
-app.use("/subcategories", subcategoriesRouter)
-app.use("/subjects", subjectsRouter)
+app.use('/categories', categoriesRouter)
+app.use('/subcategories', subcategoriesRouter)
+app.use('/subjects', subjectsRouter)
 
 //SQL connection
-const sequelize = require("./database/db")
-const { modelDefinitions } = require("./models/models")
+const sequelize = require('./database/db')
+const { modelDefinitions } = require('./models/models')
 const sqlConnect = async () => {
   try {
     await sequelize.authenticate()
-    console.log("Connection has been established successfully.")
+    console.log('Connection has been established successfully.')
     modelDefinitions()
     await sequelize.sync({ alter: true })
-    console.log("All models were synchronized successfully.")
+    console.log('All models were synchronized successfully.')
   } catch (error) {
-    console.error("Unable to connect to the database:", error)
+    console.error('Unable to connect to the database:', error)
   }
 }
 sqlConnect()
